e5b5762ff3fdeefdd2751f97d806541238b3caa9
OWONO ELOUNDOU <OWONO ELOUNDOU>
2024-05-12 18:51:43 +0100

    first commit

---
+++ assets/JS/script.js
@@ -0,0 +1,40 @@
+const images = document.querySelectorAll('.header-slider ul img');
+const prevBtn = document.querySelector('.control_prev');
+const nextBtn = document.querySelector('.control_next');
+
+let n = 0;
+
+changeSlide = () => {
+    for (let i = 0; i < images.length; i++) {
+        images[i].style.display = 'none';  
+    }
+    images[n].style.display = 'block';
+}
+
+changeSlide();
+
+prevBtn.addEventListener('click', (e) => {
+    if (n > 0) {
+        n--;
+    } else {
+        n = images.length - 1;
+    }
+    changeSlide();
+});
+
+nextBtn.addEventListener('click', (e) => {
+    if (n < images.length - 1) {
+        n++;
+    } else {
+        n = 0;
+    }
+    changeSlide();
+});
+
+const scrollContainer = document.querySelectorAll('.products');
+for (const items of scrollContainer) {
+    items.addEventListener('wheel', (evt) => {
+        evt.preventDefault;
+        items.scrollLeft += evt.deltaY;
+    })
+}
